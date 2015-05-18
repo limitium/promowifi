bedba861d350ea360a4e752e654047e89363d3d8
limitium <limitium@gmail.com>
2015-05-18 22:51:08 +0300

    fos userbundle

---
+++ backend/src/loc2me/OfferBundle/Entity/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace loc2me\OfferBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+}
